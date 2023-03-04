cf4a0928e6cbd2d14b5f0f725163bd74e05fa2f2
2bit2bit <emharuna2020@gmail.com>
2023-03-04 18:59:20 +0100

    refactor directory, use ejs view engine to render pages

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to our restaurant chatbot");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
